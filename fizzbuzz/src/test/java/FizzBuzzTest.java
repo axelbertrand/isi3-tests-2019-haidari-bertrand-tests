248255eff7dd4f720369e67c2a0b4e182a06cdf0
axelbertrand <axel.bertrand2@etu.univ-lyon1.fr>
2019-05-20 13:47:28 +0200

    Ajout des tests 1 par 1

--- fizzbuzz/src/test/java/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/FizzBuzzTest.java
@@ -34,6 +34,7 @@ class FizzBuzzTest {
         main = new FizzBuzz(length);
 
         List<String> fizzBuzz = main.fizzBuzz();
+        fizzBuzz.stream().forEach(System.out::println);
         for(int i = 0; i < length; i++) {
             if(i == 42) {
                 assertEquals("La grande question sur la vie, l'univers et le reste", fizzBuzz.get(i));
@@ -52,6 +53,76 @@ class FizzBuzzTest {
             }
             else {
                 assertEquals(i, Integer.parseInt(fizzBuzz.get(i)));
+            }
+        }
+    }
+
+    @ParameterizedTest
+    @DisplayName("should display \"Fizz\" for multiples of 3")
+    @ValueSource(ints = { 0, 1, 10, 100 })
+    void testFizzBuzzModulo3(int length) {
+        main = new FizzBuzz(length);
+
+        List<String> fizzBuzz = main.fizzBuzz();
+        for(int i  = 0; i < length; i++) {
+            if(i % 3 == 0 && i != 42) {
+                assertEquals("Fizz", fizzBuzz.get(i));
+            }
+        }
+    }
+
+    @ParameterizedTest
+    @DisplayName("should display \"Buzz\" for multiples of 5")
+    @ValueSource(ints = { 0, 1, 10, 100 })
+    void testFizzBuzzModulo5(int length) {
+        main = new FizzBuzz(length);
+
+        List<String> fizzBuzz = main.fizzBuzz();
+        for(int i  = 0; i < length; i++) {
+            if(i % 5 == 0 && i % 3 != 0 && i % 7 != 0) {
+                assertEquals("Buzz", fizzBuzz.get(i));
+            }
+        }
+    }
+
+    @ParameterizedTest
+    @DisplayName("should display \"Fizz Buzz\" for multiples of 3 and 5")
+    @ValueSource(ints = { 0, 1, 10, 100 })
+    void testFizzBuzzModulo3and5(int length) {
+        main = new FizzBuzz(length);
+
+        List<String> fizzBuzz = main.fizzBuzz();
+        for(int i  = 0; i < length; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                assertEquals("Fizz Buzz", fizzBuzz.get(i));
+            }
+        }
+    }
+
+    @ParameterizedTest
+    @DisplayName("should display \"It's a trap\" for multiples of 7")
+    @ValueSource(ints = { 0, 1, 10, 100 })
+    void testFizzBuzzModulo7(int length) {
+        main = new FizzBuzz(length);
+
+        List<String> fizzBuzz = main.fizzBuzz();
+        for(int i  = 0; i < length; i++) {
+            if(i % 7 == 0) {
+                assertEquals("It's a trap", fizzBuzz.get(i));
+            }
+        }
+    }
+
+    @ParameterizedTest
+    @DisplayName("should display \"La grande question sur la vie, l'univers et le reste\" for 42")
+    @ValueSource(ints = { 0, 1, 10, 100 })
+    void testFizzBuzz42(int length) {
+        main = new FizzBuzz(length);
+
+        List<String> fizzBuzz = main.fizzBuzz();
+        for(int i  = 0; i < length; i++) {
+            if(i == 42) {
+                assertEquals("La grande question sur la vie, l'univers et le reste", fizzBuzz.get(i));
             }
         }
     }
